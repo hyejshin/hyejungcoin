24f429ecc6b2fd471b92258fe768e334547cf8ff
hyejshin <hyejungshin7@gmail.com>
2021-07-25 23:28:44 -0700

    hyejung coin

---
+++ main.js
@@ -0,0 +1,64 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "07/25/2021", "Genesis block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let hyejungCoin = new Blockchain();
+hyejungCoin.addBlock(new Block(1, "07/26/2017", { amount: 3}));
+hyejungCoin.addBlock(new Block(1, "07/27/2017", { amount: 7}));
+
+console.log('Is blockchain valid? ' + hyejungCoin.isChainValid())
+
+hyejungCoin.chain[1].data = { amount: 100};
+hyejungCoin.chain[1].hash = hyejungCoin.chain[1].calculateHash();
+console.log('Is blockchain valid? ' + hyejungCoin.isChainValid())
+
+//console.log(JSON.stringify(hyejungCoin, null, 4));
